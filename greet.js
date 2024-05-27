fde067294f4e565e0b37413850df4935c47e5960
Jagruthi321 <119727244+Jagruthi321@users.noreply.github.com>
2024-05-27 16:19:07 +0530

    greet.js

---
+++ greet.js
@@ -0,0 +1,22 @@
+import React from "react";
+
+function Greet() {
+    return (
+        <div>
+            <h2>Hi this is functional component without props</h2>
+        </div>
+    );
+}
+
+function GreetWithProps(Props) {
+    return (
+        <div>
+            <h2>This is {Props.name}!</h2>
+            <h2>This is functional component with props</h2>
+            <hr></hr>
+        </div>
+    );
+}
+
+export default GreetWithProps;
+export { Greet };
